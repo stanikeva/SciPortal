f76a29b967ceb06a4a6a7cc5400b8a6b2e02a50a
Anna Vordou <annavordou@csd.auth.gr>
2022-05-02 19:13:11 +0300

    Clean up

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,12 @@ import("account.html")
         var z = document.getElementById(c);
         if (x.style.display == "none")
         {
-        x.style.display = "block";
-        y.style.display = "none";
-        z.style.display = "none";
+            x.style.display = "block";
+            y.style.display = "none";
+            z.style.display = "none";
         }
         else
         {
-        x.style.display = "none";
+            x.style.display = "none";
         }
     }
