01079d4b18de4602edb05accbdd9caff6be75e23
Anna Vordou <annavordou@csd.auth.gr>
2022-05-21 19:45:32 +0300

    close button in sign up lol

--- modals.php
+++ modals.php
@@ -50,7 +50,7 @@ include "test.php";
         <div class="modal-content">
             <div class="modal-header">
                 <h5 class="modal-title" id="ModalLabel2">Sign Up</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-dismiss="modal" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
                 <form action="signupsql.php" METHOD="post">
